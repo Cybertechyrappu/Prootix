c9746f478b2927e4dd4bba795c932f3c2eeb656c
qorvode.ai <rappuplayzpro@gmail.com>
2026-05-11 12:19:18 +0000

    fix: Clean Flutter Android v2 embedding setup
    - Exactly matches flutter create --platforms=android output
    - android:name=${applicationName} (NOT io.flutter.app.FlutterApplication)
    - flutterEmbedding meta-data set to value=2
    - MainActivity extends io.flutter.embedding.android.FlutterActivity
    - No GeneratedPluginRegistrant calls (handled automatically)
    - Proper Gradle, AGP, Kotlin versions

--- apps/flutter/android/app/src/main/kotlin/com/qorvode/prootix/MainActivity.kt
+++ apps/flutter/android/app/src/main/kotlin/com/qorvode/prootix/MainActivity.kt
@@ -1,10 +1,5 @@
 package com.qorvode.prootix
 
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
 
-class MainActivity : FlutterActivity() {
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-    }
-}
+class MainActivity: FlutterActivity()
