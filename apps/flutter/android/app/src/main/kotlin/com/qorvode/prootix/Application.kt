b004f8286739324790e407422d8f36f32aae930b
qorvode.ai <rappuplayzpro@gmail.com>
2026-05-11 11:49:59 +0000

    fix: Complete rebuild of android directory with proper Flutter v2 embedding

--- apps/flutter/android/app/src/main/kotlin/com/qorvode/prootix/Application.kt
+++
@@ -1,9 +0,0 @@
-package com.qorvode.prootix
-
-import io.flutter.embedding.android.FlutterApplication
-
-class Application : FlutterApplication() {
-    override fun onCreate() {
-        super.onCreate()
-    }
-}
